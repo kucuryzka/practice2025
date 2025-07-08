d88162f92de7149f68e247d39ce3c64810802630
Olesya <olegha2000@gmail.com>
2025-07-09 00:54:54 +0600

    fix: removed unnecesary lines

--- MetadataPrinterTests/MetadataPrinterTests.cs
+++ MetadataPrinterTests/MetadataPrinterTests.cs
@@ -13,31 +13,22 @@ public class MetadataPrinterTests
     public void NoArgumentTest()
     {
         StringWriter stringWriter = new StringWriter();
-        TextWriter textWriter = Console.Out;
         Console.SetOut(stringWriter);
 
         MetadataPrinter.Main(Array.Empty<string>());
         var output = stringWriter.ToString();
         Assert.Contains("path not found", output);
-
-
-        Console.SetOut(textWriter);
-        stringWriter.Dispose();
     }
 
     [Fact]
     public void NonExistingFileTest()
     {
         StringWriter stringWriter = new StringWriter();
-        TextWriter textWriter = Console.Out;
+        
         Console.SetOut(stringWriter);
 
         MetadataPrinter.Main(new[] { "somefile.dll" });
         var output = stringWriter.ToString();
         Assert.Contains("file not found", output);
-
-
-        Console.SetOut(textWriter);
-        stringWriter.Dispose();
     }
 }
