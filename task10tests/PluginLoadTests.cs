689a8e8726b21420d8f055dda60901c05c7e94aa
Olesya <olegha2000@gmail.com>
2025-07-10 18:52:29 +0600

    feat: PluginLoader + tests

---
+++ task10tests/PluginLoadTests.cs
@@ -0,0 +1,53 @@
+using Xunit;
+using System.IO;
+using task10;
+
+
+namespace task10tests;
+
+public class PluginLoadTests
+{
+
+    [Fact]
+    public void NonExistentDirectory_Test()
+    {
+        var string_writer = new StringWriter();
+        Console.SetOut(string_writer);
+
+        var loader = new PluginLoader(@"\some\directory");
+        loader.FindPlugins();
+
+        var output = string_writer.ToString();
+        Assert.Contains("directory not found", output);
+
+    }
+
+    [Fact]
+    public void PluginLoaderLoadsSamplePlugin_Test()
+    {
+        var string_writer = new StringWriter();
+        Console.SetOut(string_writer);
+
+        var loader = new PluginLoader(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+        loader.FindPlugins();
+
+        var output = string_writer.ToString();
+        Assert.Contains("sample plugin", output);
+    }
+
+    [Fact]
+    public void PluginLoaderProcessesDependenciesCorrect_Test()
+    {
+        var string_writer = new StringWriter();
+        Console.SetOut(string_writer);
+
+        var loader = new PluginLoader(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+        loader.FindPlugins();
+
+        var output = string_writer.ToString().Split(Environment.NewLine);
+
+        Assert.Equal("sample plugin", output[0]);
+        Assert.Equal("dependant plugin", output[1]);
+
+    }
+}
