0aec86763f9fcffc8bce9eb3fd13e8beb5229ce5
Olesya <olegha2000@gmail.com>
2025-07-19 13:17:48 +0600

    feat: test command implementation + tests and reports

--- ServerCommands/ServerCommands.cs
+++ ServerCommands/ServerCommands.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 namespace ServerCommands;
 
 
@@ -7,6 +8,45 @@ public interface ICommand
     void Execute();
 }
 
+public class TestCommand : ICommand
+{
+    public int id;
+    private readonly IScheduler scheduler;
+    public int total_count;
+    public int counter = 0;
+    public long execution_time;
+    private Stopwatch sw = new Stopwatch();
+
+    public TestCommand(int id, IScheduler scheduler, int total_count)
+    {
+        this.id = id;
+        this.scheduler = scheduler;
+        this.total_count = total_count;
+    }
+
+
+    public void Execute()
+    {
+        if (counter == total_count) return;
+        if (counter == 0)
+            sw.Start();
+
+        counter++;
+        Console.WriteLine($"thread: {id} call: {counter}");
+
+        Thread.Sleep(10);
+
+        if (counter < total_count)
+        {
+            scheduler.Add(this);
+        }
+        else
+        {
+            sw.Stop();
+            execution_time = sw.ElapsedMilliseconds;
+        }
+    }
+}
 
 public interface IScheduler
 {
---
+++ task19/Program19.cs
@@ -0,0 +1,47 @@
+﻿using ServerCommands;
+using task17;
+
+namespace task19;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int commandQuantity = 5;
+        int commandCall = 3;
+
+        var scheduler = new Scheduler();
+        var server = new ServerThread(scheduler);
+
+        var commands = new List<TestCommand>();
+        for (int i = 1; i < commandQuantity + 1; i++)
+        {
+            var cmd = new TestCommand(i, scheduler, commandCall);
+            scheduler.Add(cmd);
+            commands.Add(cmd);
+        }
+
+
+        while (commands.Any(c => c.counter < commandCall))
+            Thread.Sleep(10);
+
+        server.AddCommand(new HardStop(server));
+        Thread.Sleep(1000);
+
+        double[] executionTimes = commands.Select(cmd => (double)cmd.execution_time).ToArray();
+        double[] commandId = { 1, 2, 3, 4, 5 };
+
+        var plt = new ScottPlot.Plot();
+        plt.XLabel("command quantity");
+        plt.YLabel("time(ms)");
+        plt.Add.Scatter(commandId, executionTimes);
+
+        plt.SavePng(@"C:\practice2025\plot19.png", 600, 300);
+
+        double totalExecutionTime = executionTimes.Sum();
+
+        string res = $"Command quantity: 5, every command was called 3 times\n Total execution time: {totalExecutionTime}";
+        File.WriteAllText(@"C:\practice2025\result19.txt", res);
+
+    }
+}
